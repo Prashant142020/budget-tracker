369e12b9205705e6b9a463503c363becb63715df
Prashant <prashantkumar142020@gmail.com>
2024-05-04 18:53:39 +0530

    add Logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { SignIn } from "./(auth)/sign-in";
+import SignOut from "./(auth)/sign-out";
+import Logo from "@/components/Logo";
  
 
 export default function Home() {
@@ -8,6 +10,8 @@ export default function Home() {
       <h1> hi prashant</h1>
       <Button>Click me</Button>
       <SignIn />
+      <SignOut />
+      <Logo />
     </main>
   );
 }
